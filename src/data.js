53448621ffd8a8d47ab17926de60cc5a0526be8e
he2e2 <dddd6368@naver.com>
2024-08-14 15:15:07 +0900

    feat: Add renderPagination function

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
-let start = 1;
+let totalItems;
+const postsPerPage = 10;
+const pagesPerGroup = 5;
 
-let url = `http://openapi.seoul.go.kr:8088/735a4d656177686734374978656774/json/ListPublicReservationEducation/${start}/${
-  start + 9
-}/`;
-let totalLength;
+let currentPage = 1;
+let currentGroup = 1;
+let totalPages = Math.ceil(totalItems / postsPerPage);
 
 const createItem = (event) => {
   return `
@@ -42,23 +43,80 @@ const createItem = (event) => {
     `;
 };
 
-const fetchData = async () => {
+const fetchData = async (url) => {
   return await fetch(url)
     .then((res) => res.json())
     .then((data) => {
       data = data.ListPublicReservationEducation;
-      totalLength = data.list_total_count;
+      totalItems = data.list_total_count;
+      totalPages = Math.ceil(totalItems / postsPerPage);
 
       return data.row;
     });
 };
 
-const renderItem = async () => {
+const renderItem = async (start = 1) => {
   const $listWrapper = document.querySelector(".list-wrapper");
-  const data = await fetchData();
+  $listWrapper.innerHTML = "";
+  const data = await fetchData(
+    `http://openapi.seoul.go.kr:8088/735a4d656177686734374978656774/json/ListPublicReservationEducation/${start}/${
+      start + 9
+    }/`
+  );
 
   const itemHtml = data.map((row) => createItem(row)).join("");
   $listWrapper.innerHTML = itemHtml;
+  renderPagination();
 };
 
-renderItem();
+const renderPagination = () => {
+  if (totalItems <= 10) return;
+
+  const $pagination = document.querySelector(".pagination");
+  $pagination.innerHTML = "";
+
+  const startPage = (currentGroup - 1) * pagesPerGroup + 1;
+  const endPage = Math.min(startPage + pagesPerGroup - 1, totalPages);
+
+  if (currentGroup > 1) {
+    const prevGroupButton = document.createElement("i");
+    prevGroupButton.className = "fa-solid fa-chevron-left";
+    prevGroupButton.addEventListener("click", () => {
+      currentGroup--;
+      renderPagination();
+    });
+    $pagination.appendChild(prevGroupButton);
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    const pageButton = document.createElement("button");
+    pageButton.textContent = i;
+    pageButton.className = i === currentPage ? "on" : "";
+    pageButton.addEventListener("click", () => {
+      currentPage = i;
+      renderItem((currentPage - 1) * 10 + 1);
+      renderPagination();
+    });
+    $pagination.appendChild(pageButton);
+  }
+
+  if (currentGroup * pagesPerGroup < totalPages) {
+    const nextGroupButton = document.createElement("i");
+    nextGroupButton.className = "fa-solid fa-chevron-right";
+    nextGroupButton.addEventListener("click", () => {
+      currentGroup++;
+      renderPagination();
+    });
+    $pagination.appendChild(nextGroupButton);
+  }
+};
+
+const renderDetails = () => {
+  // todo
+};
+
+const init = async () => {
+  renderItem();
+};
+
+// init();
