610b67b571c98bb7c915c9a23ed9d3f1d75a96ba
he2e2 <dddd6368@naver.com>
2024-08-15 23:09:29 +0900

    feat: Add map href

--- src/map.js
+++ src/map.js
@@ -1,34 +1,34 @@
 export const renderingMap = (element, places) => {
-  let mapOption = {
+  const mapOption = {
     center: new kakao.maps.LatLng(37.5665, 126.978),
     level: 8,
   };
 
-  let map = new kakao.maps.Map(element, mapOption);
+  const map = new kakao.maps.Map(element, mapOption);
   createMarker(places, map, "main");
 };
 
 export const renderingDetailMap = (element, place) => {
-  let mapOption = {
+  const mapOption = {
     center: new kakao.maps.LatLng(place[0].Y, place[0].X),
     level: 3,
   };
 
-  let map = new kakao.maps.Map(element, mapOption);
+  const map = new kakao.maps.Map(element, mapOption);
   createMarker(place, map, "detail");
 };
 
 const createMarker = (places, map, type) => {
-  let imageSrc = "../img/marker.svg";
+  const imageSrc = "../img/marker.svg";
 
   places.map((place) => {
-    let imageSize = new kakao.maps.Size(40, 40);
-    let imageOffset = new kakao.maps.Point(20, 40);
-    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, {
+    const imageSize = new kakao.maps.Size(40, 40);
+    const imageOffset = new kakao.maps.Point(20, 40);
+    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, {
       offset: imageOffset,
     });
 
-    let marker = new kakao.maps.Marker({
+    const marker = new kakao.maps.Marker({
       map: map,
       position: new kakao.maps.LatLng(place.Y, place.X),
       title: place.PLACENM,
@@ -36,17 +36,20 @@ const createMarker = (places, map, type) => {
     });
 
     if (type === "detail") {
-      let content = `
+      const mapLink = `https://map.kakao.com/link/map/${place.Y},${place.X}`;
+      const content = `
         <div class="overlay">
+          <a href="${mapLink}" target="_blank">
             <img src="${place.IMGURL}" alt="thumbnail" />
             <div>
                 <p class="service-name">${place.SVCNM}</p>
                 <span class="place-name">${place.PLACENM}</span>
             </div>
+          </a>
         </div>
     `;
 
-      let customOverlay = new kakao.maps.CustomOverlay({
+      const customOverlay = new kakao.maps.CustomOverlay({
         position: new kakao.maps.LatLng(place.Y, place.X),
         content: content,
         yAnchor: 1,
